c7fb6b02b75287d963452033f9530049d9e61259
Luke Captain <luke@thebeautybuddy.com>
2020-03-19 14:47:48 +0000

    round rect test

--- src/ios/CDVBarcodeScanner.mm
+++ src/ios/CDVBarcodeScanner.mm
@@ -953,6 +953,29 @@ parentViewController:(UIViewController*)parentViewController
     [self resizeElements];
     return overlayView;
 }
+//-------------------------------------------------------------------------
+// Round rect
+void addRoundedRectToPath(CGContextRef context, CGRect rect, float ovalWidth, float ovalHeight);
+{
+    float fw, fh;
+    if (ovalWidth == 0 || ovalHeight == 0) {
+        CGContextAddRect(context, rect);
+        return;
+    }
+    CGContextSaveGState(context);
+    CGContextTranslateCTM (context, CGRectGetMinX(rect), CGRectGetMinY(rect));
+    CGContextScaleCTM (context, ovalWidth, ovalHeight);
+    fw = CGRectGetWidth (rect) / ovalWidth;
+    fh = CGRectGetHeight (rect) / ovalHeight;
+    CGContextMoveToPoint(context, fw, fh/2);
+    CGContextAddArcToPoint(context, fw, fh, fw/2, fh, 1);
+    CGContextAddArcToPoint(context, 0, fh, 0, fh/2, 1);
+    CGContextAddArcToPoint(context, 0, 0, fw/2, 0, 1);
+    CGContextAddArcToPoint(context, fw, 0, fw, fh/2, 1);
+    CGContextClosePath(context);
+    CGContextRestoreGState(context);
+}
+
 
 //--------------------------------------------------------------------------
 
@@ -980,17 +1003,19 @@ parentViewController:(UIViewController*)parentViewController
     }
 
     if (self.processor.is2D) {
-        UIColor* color = [UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:RETICLE_ALPHA];
+        UIColor* color = [UIColor colorWithRed:0.0 green:0.4 blue:0.6 alpha:RETICLE_ALPHA];
         CGContextSetStrokeColorWithColor(context, color.CGColor);
         CGContextSetLineWidth(context, RETICLE_WIDTH);
-        CGContextStrokeRect(context,
-                            CGRectMake(
+        CGRect roundRect =  CGRectMake(
                                        RETICLE_OFFSET,
                                        RETICLE_OFFSET,
                                        RETICLE_SIZE-2*RETICLE_OFFSET,
                                        RETICLE_SIZE-2*RETICLE_OFFSET
-                                       )
-                            );
+                                       );
+        addRoundedRectToPath(context, roundRect, 5, 5);
+//        CGContextStrokeRect(context,
+//
+//                            );
     }
 
     result = UIGraphicsGetImageFromCurrentImageContext();
