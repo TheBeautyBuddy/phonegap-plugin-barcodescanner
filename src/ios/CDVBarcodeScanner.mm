b790d4a9b90c4bcf70be492539994f59a94e96b3
Luke Captain <luke@thebeautybuddy.com>
2020-03-20 12:41:12 +0000

    change colour to white

--- src/ios/CDVBarcodeScanner.mm
+++ src/ios/CDVBarcodeScanner.mm
@@ -980,7 +980,7 @@ parentViewController:(UIViewController*)parentViewController
     }
 
     if (self.processor.is2D) {
-        UIColor* color = [UIColor colorWithRed:0.0 green:0.4 blue:0.6 alpha:RETICLE_ALPHA];
+        UIColor* color = [UIColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:RETICLE_ALPHA];
         CGContextSetStrokeColorWithColor(context, color.CGColor);
         CGContextSetLineWidth(context, RETICLE_WIDTH);
         CGContextStrokeRect(context,
